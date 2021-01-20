36530eca8f34bf2a9b5191964ffe1797632ebad2
belccarvalho <belccarvalho@gmail.com>
2021-01-20 11:43:33 +0100

    substitute app to server.js

--- server.js
+++ server.js
@@ -1,8 +1,64 @@
+var express = require("express");
+var createError = require("http-errors");
 require("dotenv").config();
 const http = require("http");
-const app = require("./app");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+let mongoose = require("mongoose");
+let cors = require("cors");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-const PORT = process.env.PORT;
+var app = express();
 
+const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log("listening to port:", PORT);
+});
+
+//make connection to db
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+//make a reference to database
+let db = mongoose.connection;
+db.on("error", () => {
+  console.log("connection error");
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
